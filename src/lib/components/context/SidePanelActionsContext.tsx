dcb3be640ed446552edecfee48409d9cc882661f
Tobi <stevehappyboy@rocketmail.com>
2021-05-04 15:13:58 +0100

    SidePanelActionsContext: update actions to return Promise

--- src/lib/components/context/SidePanelActionsContext.tsx
+++ src/lib/components/context/SidePanelActionsContext.tsx
@@ -9,25 +9,25 @@ export interface SidePanelActionsContextProps{
    * @param item
    */
   replace: (item: SidePanelItem) => void;
-  pop: (force?: boolean) => boolean;
+  pop: (force?: boolean) => Promise<boolean>;
   /**
    * Pop stack until the specified panel id is reached. Returns true if the action is successful
    * @param id
    * @param force
    */
-  popTo: (id: string, force?: boolean) => boolean;
+  popTo: (id: string, force?: boolean) => Promise<boolean>;
   /**
    * Clears the stack to show only the default component. Returns true if the action is successful
    * @param force
    */
-  reset: (force?: boolean) => boolean;
+  reset: (force?: boolean) => Promise<boolean>;
   getItems: () => SidePanelItem[];
   /**
    * clear entire stack and push the specified panel. Returns true if the action is successful
    * @param item
    * @param force
    */
-  resetTo: (item: SidePanelItem, force?: boolean) => boolean;
+  resetTo: (item: SidePanelItem, force?: boolean) => Promise<boolean>;
   collapse: (flag: boolean) => void;
   toggleCollapse: () => void;
   ref: React.RefObject<PanelPageEvents|null>;
